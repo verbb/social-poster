18afe7d61fe989b5e73c54decaed6f1431ae5fe4
Josh Crawford <joshua@sgroup.com.au>
2016-07-11 08:56:31 +1000

    version 1.1.2

--- socialposter/SocialPosterPlugin.php
+++ socialposter/SocialPosterPlugin.php
@@ -14,7 +14,7 @@ class SocialPosterPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.1.0';
+        return '1.1.2';
     }
 
     public function getSchemaVersion()
