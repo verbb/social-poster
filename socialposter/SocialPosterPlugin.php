f39bd94180954bb4da3d7b52e1af3d7c9bd8ef86
Josh Crawford <Josh@engramdesign.com.au>
2017-11-05 16:34:00 +1100

    Version 1.2.2

--- socialposter/SocialPosterPlugin.php
+++ socialposter/SocialPosterPlugin.php
@@ -14,7 +14,7 @@ class SocialPosterPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.2.1';
+        return '1.2.2';
     }
 
     public function getSchemaVersion()
