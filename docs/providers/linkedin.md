bbe78f94e080041c0632d37457ff61746a792b2a
Josh Crawford <joshua@sgroup.com.au>
2023-10-06 21:11:24 +1100

    Change LinkedIn from using deprecated “Shares API”

--- docs/providers/linkedin.md
+++ docs/providers/linkedin.md
@@ -5,7 +5,12 @@ Follow these steps to configure LinkedIn for Social Poster.
 1. Go to <a href="https://www.linkedin.com/developers/apps/new" target="_blank">LinkedIn Developer Apps</a> and login to your account.
 1. Click the **Create App** button and complete all the required fields.
 1. Navigate to the **Products** section.
-1. Click the **Select** button for the **Sign In with LinkedIn using OpenID Connect** product.
+1. If you want to post to a personal LinkedIn Page:
+    1. Click the **Request Access** button for the **Sign In with LinkedIn using OpenID Connect** product.
+    1. Click the **Request Access** button for the **Share on LinkedIn** product.
+1. If you want to post to a LinkedIn Company Page:
+    1. Click the **Request Access** button for the **Community Management API** product.
+        - You will be required to authenticate with your Company, and submit an access request form.
 1. Navigate to the **Auth** section.
 1. Click the edit icon for the **Authorized Redirect URLs** field.
 1. Enter the value from the **Redirect URI** field in Social Poster.
@@ -13,40 +18,9 @@ Follow these steps to configure LinkedIn for Social Poster.
 1. Copy the **Client Secret** from LinkedIn and paste in the **Client Secret** field in Social Poster.
 
 
-## Company Pages
-In order to post to company pages, there are a few requirements:
-
-1. Ensure your LinkedIn app is correctly linked and verified to the company page in your app **Settings** tab.
-1. The LinkedIn account used to authorise with Social Poster must be an admin of the company.
-1. Have [Marketing Developer Platform](https://business.linkedin.com/marketing-solutions/case-studies/businessonline) permissions (see below).
-
-### Marketing Developer Platform
-To gain permissions for posting to company pages, you must sign up for the [Marketing Developer Platform](https://business.linkedin.com/marketing-solutions/case-studies/businessonline).
-
-Go to the **Products** tab in your LinkedIn app and click **Add more products**. Check the **Marketing Developer Platform** checkbox and follow the prompts to fill out your application. Please note that the approval process can take up to 20-25 days. Please be aware that LinkedIn will not approve every application submitted.
+## Company Pages vs Personal Pages
+If you want to post to **only** personal pages, you will only require the **Share on LinkedIn** product with your LinkedIn app. If you want to post to a Company page, or both a Personal _and_ Company page, you'll need to apply for the **Community Management API** product.
 
 Once approved, verify you have the correct permissions via the **Auth** tab. Ensure you have `w_organization_social` and `r_organization_social` permissions. You should now be able to post to LinkedIn company pages.
 
-If you try to post to a company page without these permissions, you'll likely receive a permission error.
-
-### Posting to both Company and Personal pages
-If you want to be able to post to **both** company and personal pages, you'll need to include the following scopes in your request to authenticate with the LinkedIn app. Be sure to also follow the above steps to ensure your app has the correct permissions.
-
-If you haven't already done so, create a `social-poster.php` file in your `/config` directory. See the [config docs](docs:get-started/configuration) for more information.
-
-Use the following for LinkedIn:
-
-```php
-'linkedin' => [
-    'clientId' => 'xxxxxxxxxxxx',
-    'clientSecret' => 'xxxxxxxxxxxx',
-
-    'scope' => [
-        'w_organization_social',
-        'r_organization_social',
-    ],
-],
-```
-
-Including these scopes tells LinkedIn to authorise Social Poster to use these scopes. This is done automatically if you want to post to **just** the company page, but not if you want to use both. This should allow you to authorise against both possibilities.
-
+For posting to Personal pages, only the `w_member_social` permission is required.
--- src/accounts/LinkedIn.php
+++ src/accounts/LinkedIn.php
@@ -45,18 +45,44 @@ class LinkedIn extends OAuthAccount
         return $rules;
     }
 
+    public function getOAuthProviderConfig(): array
+    {
+        $config = parent::getOAuthProviderConfig();
+
+        // Use the "Posts API"
+        // https://learn.microsoft.com/en-us/linkedin/marketing/integrations/community-management/shares/posts-api
+        $config['restProtocolVersion'] = '2.0.0';
+        $config['restVersion'] = '202309';
+
+        // We need to reset the default scopes, because different ones are required depending on whether
+        // posting to a personal page, or an organization. This is because the "Community Management API"
+        // which is needed to post to an organisation can be the only product for a LinkedIn app.
+        $config['defaultScopes'] = [];
+
+        return $config;
+    }
+
     public function getDefaultScopes(): array
     {
-        $scopes = [
-            'w_member_social',
-        ];
-
-        if ($this->endpoint == 'organization') {
-            $scopes[] = 'w_organization_social';
-            $scopes[] = 'r_organization_social';
+        // Posting to a personal page can be done with the "Share on LinkedIn" which doesn't require application.
+        if ($this->endpoint === 'person') {
+            return [
+                'openid',
+                'email',
+                'profile',
+                'w_member_social',
+            ];
         }
 
-        return $scopes;
+        // Posting to a company page requires the "Community Management API" permission.
+        if ($this->endpoint === 'organization') {
+            return [
+                'w_organization_social',
+                'r_organization_social',
+            ];
+        }
+
+        return [];
     }
 
     public function getResponseUrl($data): ?string
@@ -71,43 +97,66 @@ class LinkedIn extends OAuthAccount
             $endpoint = $this->endpoint;
 
             if ($endpoint === 'person') {
-                $response = $this->request('GET', 'me');
-                $profileId = $response['id'] ?? null;
+                $response = $this->request('GET', 'userinfo');
+                $profileId = $response['sub'] ?? null;
 
-                $ownerUrn = 'urn:li:person:' . $profileId;
+                $ownerUrn = 'urn:li:person:' . $profileId;  
             } else if ($endpoint === 'organization') {
                 $ownerUrn = 'urn:li:organization:' . $this->organizationId;
             }
 
-            $thumbnails = [];
+            $content = [
+                'author' => $ownerUrn,
+                'commentary' => $payload->message,
+                'visibility' => 'PUBLIC',
+                'lifecycleState' => 'PUBLISHED',
+                'isReshareDisabledByAuthor' => false,
+                'distribution' => [
+                    'feedDistribution' => 'MAIN_FEED',
+                    'targetEntities' => [],
+                    'thirdPartyDistributionChannels' => [],
+                ],
+            ];
 
-            if ($payload->picture) {
-                $thumbnails[] = [
-                    'resolvedUrl' => $payload->picture,
+            // If we're sharing a URL, make it an article. Can only be either article or image though
+            if ($payload->url && !$payload->picture) {
+                $content['content']['article'] = [
+                    'source' => $payload->url,
+                    'title' => $payload->title,
+                    'description' => $payload->message,
                 ];
             }
 
-            $response = $this->sendRequest($payload->element, 'shares', [
-                'content' => [
-                    'contentEntities' => [
-                        [
-                            'entityLocation' => $payload->url,
-                            'thumbnails' => $thumbnails,
+            if ($payload->picture) {
+                // Images are required to be uploaded first via 2 separate API calls.
+                // Generate an asset request first, which will tell us where to upload and the Asset URN.
+                $response = $this->request('POST', 'https://api.linkedin.com/rest/images', [
+                    'query' => [
+                        'action' => 'initializeUpload',
+                    ],
+                    'json' => [
+                        'owner' => $ownerUrn,
+                        'initializeUploadRequest' => [
+                            'owner' => $ownerUrn,
                         ],
                     ],
-                    'title' => $payload->title,
-                ],
-                'owner' => $ownerUrn,
-                'subject' => $payload->title,
-                'text' => [
-                    'text' => $payload->message,
-                ],
-                'distribution' => [
-                    'linkedInDistributionTarget' => [
-                        'visibleToGuest' => true,
-                    ],
-                ],
-            ]);
+                ]);
+
+                $uploadUrl = $response['value']['uploadUrl'] ?? null;
+                $imageUrn = $response['value']['image'] ?? null;
+
+                if ($uploadUrl && $imageUrn) {
+                    // Upload the image itself
+                    $response = $this->request('POST', $uploadUrl, [
+                        'body' => file_get_contents($payload->picture),
+                    ]);
+
+                    $content['content']['media']['id'] = $imageUrn;
+                }
+            }
+
+            // We have to use the full URL here, the client will assume `api.linkedin.com/v2`.
+            $response = $this->sendRequest($payload->element, 'https://api.linkedin.com/rest/posts', $content);
 
             return $this->getPostResponse($response);
         } catch (Throwable $e) {
